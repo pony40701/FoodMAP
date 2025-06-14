1dba80a6985dfc352a1508640596f80490a3ce5e
pony40701 <pony40701@gmail.com>
2025-06-14 23:05:19 +0800

    0614 2305

--- backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,29 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import com.example.demo.entity.GoogleRestaurant;
+import com.example.demo.repository.GoogleRestaurantRepository;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private GoogleRestaurantRepository googleRestaurantRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        List<GoogleRestaurant> restaurants = googleRestaurantRepository.findAll();
+        for (GoogleRestaurant r : restaurants) {
+            System.out.println("ID: " + r.getId());
+        }
     }
 }
