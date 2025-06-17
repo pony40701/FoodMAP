460249dbda2ff21e265d6deeecf7ade0955a5c0b
LiuSsuHsien <vc15jk05@gmail.com>
2025-06-17 14:33:30 +0800

    0617 上傳完成：restaurantList相關功能整合

--- backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
-
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections; // 如果你也用到了 Collections.emptyList()
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
