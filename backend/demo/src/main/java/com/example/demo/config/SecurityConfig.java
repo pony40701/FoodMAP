3acb8b8de96afebd3508649808886fa7c0da4775
shrsheng <shrshengliao@gmail.com>
2025-06-17 14:52:52 +0800

    更新

--- backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -10,8 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
-import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
