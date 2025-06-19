3cb3db40bfde5bb6b45b3443de91cadaa5f9befd
shrsheng <shrshengliao@gmail.com>
2025-06-19 12:35:48 +0800

    修正錯誤

--- backend/demo/src/main/java/com/example/demo/entity/Review.java
+++ backend/demo/src/main/java/com/example/demo/entity/Review.java
@@ -2,16 +2,7 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
